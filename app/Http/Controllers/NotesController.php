8f2f67c33f2085a0248f93dadf42c6916b78ed1f
Ioann <ioannrv@ya.ru>
2026-05-24 17:33:28 +0300

    routers

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,38 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NotesController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function edit(Note $id)
+    {
+    }
+
+    public function update(Request $request, Note $id)
+    {
+    }
+
+    public function destroy(Note $id)
+    {
+    }
+
+    public function togglePin(Note $id)
+    {
+    }
+}
